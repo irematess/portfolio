89ecf8bd4b5b72561c10dcc8b6f16ac8c5afb336
irem ates <irematees16@gmail.com>
2024-06-17 14:20:17 +0300

    Fix size

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="flex justify-between sm:px-4 px-12 py-6 items-center text-white border-b border-gray text-nowrap">
-        <span class="text-5xl sm:text-3xl  italic font-medium font-mazius">irem ateş</span>
-        <nav class="flex gap-16 font-neutral sm:text-sm text-lg items-center">
+        <span class="text-5xl max-md:text-3xl  italic font-medium font-mazius">irem ateş</span>
+        <nav class="flex gap-16 font-neutral max-md:text-sm text-lg items-center">
             <a href="#about" class="transition hover:bg-[#2F3634] hover:py-2 hover:px-4 rounded-full ease-in hover:duration-300 duration-300">ABOUT</a>
             <a href="#project" class="transition hover:bg-[#2F3634] hover:py-2 hover:px-4 rounded-full ease-in hover:duration-300 duration-300">PROJECT</a>
             <a href="#getintouch" class="transition hover:bg-[#2F3634] hover:py-2 hover:px-4 rounded-full ease-in hover:duration-300 duration-300">GET IN TOUCH</a>
--- components/Technologies.vue
+++ components/Technologies.vue
@@ -4,7 +4,7 @@ const TECHNOLOGIES = ['Nuxt','Vue','Javascript','Tailwind', 'Bootstrap','CSS', '
 <template>
     <article>
         <div class="flex text-center justify-center text-white font-mazius mt-60 text-4xl">technologies</div>
-        <div class="mt-24 w-3/5 mx-auto grid sm:grid-cols-1 max-xl:grid-cols-2 grid-cols-3 gap-8 ">
+        <div class="mt-24 w-3/5 mx-auto grid max-md:grid-cols-1 max-xl:grid-cols-2 grid-cols-3 gap-8 ">
             <div class="rounded-3xl text-center py-24 px-8 bg-gray text-white font-neutral font-semibold" v-for="(tech,index) in TECHNOLOGIES" :key="index"> {{ tech }}</div>
         </div>
     </article>
